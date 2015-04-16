e88d8aabccbe4dc091366dcb139717cdcd87900d
Thomas Liebscher <thomas.liebscher@iconcifuture.com>
2015-04-16 23:25:55 +0200

    CPB-509 add bootstrapping for custom DB

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -1,9 +1,7 @@
 unless node['mysql'].nil?
 
-  # Web Applications
-
-  unless node['applications'].nil?
-    node['applications'].each do |name, config|
+  unless node['sites'].nil?
+    node['sites'].each do |name, config|
       execute "#{name}-import-db-struct" do
         command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/prod.struct.sql\""
         only_if do
@@ -36,10 +34,10 @@ unless node['mysql'].nil?
     end
   end
 
-  unless node['applications'].nil?
-    node['applications'].each do |name, config|
-      template "tmp/grants.#{config['name']}.sql" do
-        source "grants.sql.erb"
+  unless node['sites'].nil?
+    node['sites'].each do |name, config|
+      template "/tmp/bootstrap.#{name}.sql" do
+        source "bootstrap.sql.erb"
         owner "root"
         group "root"
         mode 00644
@@ -48,76 +46,10 @@ unless node['mysql'].nil?
           File.exists?("/var/www/#{config['fqdn']}/sql/prod.struct.sql")
         end
       end
-    end
-  end
-
-  # Services
-
-  unless node['services'].nil?
-    node['services'].each do |name, config|
       execute "#{name}-import-db-struct" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/prod.struct.sql\""
+        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/tmp/bootstrap.#{config['name']}.sql\""
         only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/prod.struct.sql")
-        end
-      end
-
-      execute "#{name}-import-db-data" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/prod.data.sql\""
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/prod.data.sql")
-        end
-      end
-
-      execute "#{name}-apply-delta" do
-        command "/var/www/#{config['fqdn']}/sql/delta.sh"
-        user "root"
-        action :run
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/delta.sh")
-        end
-      end
-
-      execute "#{name}-apply-development" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/development.sql\""
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/development.sql")
-        end
-      end
-    end
-  end
-
-  # Workers
-
-  unless node['workers'].nil?
-    node['workers'].each do |name, config|
-      execute "#{name}-import-db-struct" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/prod.struct.sql\""
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/prod.struct.sql")
-        end
-      end
-
-      execute "#{name}-import-db-data" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/prod.data.sql\""
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/prod.data.sql")
-        end
-      end
-
-      execute "#{name}-apply-delta" do
-        command "/var/www/#{config['fqdn']}/sql/delta.sh"
-        user "root"
-        action :run
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/delta.sh")
-        end
-      end
-
-      execute "#{name}-apply-development" do
-        command "\"#{node['mysql']['mysql_bin']}\" -u root #{node['mysql']['server_root_password'].empty? ? '' : '-p' }\"#{node['mysql']['server_root_password']}\" < \"/var/www/#{config['fqdn']}/sql/development.sql\""
-        only_if do
-          File.exists?("/var/www/#{config['fqdn']}/sql/development.sql")
+          File.exists?(/tmp/bootstrap.#{config['name']}.sql)
         end
       end
     end
